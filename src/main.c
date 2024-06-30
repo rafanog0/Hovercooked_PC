211eff59a5b4d16911817f5ee227e5ff4094ad4a
rafanog0 <rafanogob@gmail.com>
2024-06-30 17:23:56 -0300

    main

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "../inc/display_infos.h"
 #include "../inc/game.h"
 #include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
+
+
 
 int main() {
 
@@ -12,26 +14,28 @@ int main() {
   Difficulty_t choice = display_menu();
   if (choice.difficulty_id == 4)
     return 0;
-
+  
   pthread_t timer;
   pthread_t orders;
   pthread_t *chef;
   pthread_t manager;
   pthread_mutex_init(&order_mutex, NULL);
-  pthread_mutex_init(&ingredient_mutex, NULL);
+  pthread_mutex_init(&ingredient_mutex, NULL); 
   pthread_mutex_init(&kitchen_mutex, NULL);
   pthread_mutex_init(&info_mutex, NULL);
-  List_t *orders_list = (List_t *)malloc(sizeof(List_t));
-  if (orders_list == NULL)
+  pthread_mutex_init(&cook_mutex, NULL);
+  List_t *orders_list = (List_t*) malloc(sizeof(List_t));
+  if(orders_list == NULL)
     return ERRO;
 
   benches_n = choice.benches;
   cooks_n = choice.cooks;
 
-  chef = (pthread_t *)malloc(cooks_n * sizeof(pthread_t));
-  cooks = (cook *)malloc(cooks_n * sizeof(cook));
+  chef = (pthread_t*) malloc(cooks_n * sizeof(pthread_t));
+  cooks = (cook*) malloc(cooks_n*sizeof(cook));
 
-  for (int i = 0; i < cooks_n; i++) {
+  for(int i = 0; i < cooks_n; i++)
+  {
     strcpy(cooks[i].current_order, "NONE");
     cooks[i].order_status = NO_ORDERS;
   }
@@ -44,35 +48,12 @@ int main() {
     return ERRO;
 
   pthread_create(&timer, NULL, match_clock, (void *)orders_list);
+
   pthread_create(&orders, NULL, create_orders, (void *)orders_list);
-
-  // Essas instruções são papel do gerente (TODO: Gerente)
-  int cook_id_f = 0;
-  void **args_cooking_f = (void **)malloc(2 * sizeof(void));
-  args_cooking_f[0] = orders_list;
-  args_cooking_f[1] = &cook_id_f;
-  strcpy(cooks[0].current_order, "Pao de Queijo");
-
-  int cook_id_s = 1;
-  void **args_cooking_s = (void **)malloc(2 * sizeof(void));
-  args_cooking_s[0] = orders_list;
-  args_cooking_s[1] = &cook_id_s;
-  strcpy(cooks[1].current_order, "Bife Acebolado c Fritas");
-
-  int cook_id_x = 2;
-  void **args_cooking_x = (void **)malloc(2 * sizeof(void));
-  args_cooking_x[0] = orders_list;
-  args_cooking_x[1] = &cook_id_x;
-  strcpy(cooks[2].current_order, "Isca de Peixe");
-  // Essas instruções são papel do gerente (TODO: Gerente)
-
-  // pthread_create(&chef[0], NULL, cooking, args_cooking_f);
-  // pthread_create(&chef[1], NULL, cooking, args_cooking_s);
-  // pthread_create(&chef[2], NULL, cooking, args_cooking_x);
 
   pthread_create(&manager, NULL, managing, (void *)orders_list);
 
-  while (orders_list->time_left > 0)
+  while(orders_list->time_left > 0)
     display_game(orders_list);
 
   pthread_mutex_destroy(&order_mutex);
